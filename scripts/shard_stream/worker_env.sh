73f3ab3f48031824c176de2975260bb16a899c17
Shahyar Anfaz <shahyar.anfaz@mail.utoronto.ca>
2026-09-06 23:32:16 -0400

    Add disposable lab worker bootstrap

---
+++ scripts/shard_stream/worker_env.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Source this file before running a Net3 consumer segment.
+
+export V211_LOCAL_ROOT=${V211_LOCAL_ROOT:-/tmp/anfazsha-v211}
+export V211_REPO=${V211_REPO:-/student/anfazsha/nnue-pytorch}
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+export PYTHONDONTWRITEBYTECODE=1
+export TMPDIR="$V211_LOCAL_ROOT/tmp"
+export PIP_CACHE_DIR="$V211_LOCAL_ROOT/pip-cache"
+export TORCH_HOME="$V211_LOCAL_ROOT/torch-cache"
+export TORCHINDUCTOR_CACHE_DIR="$V211_LOCAL_ROOT/inductor-cache"
+export XDG_CACHE_HOME="$V211_LOCAL_ROOT/xdg-cache"
+export CUDA_CACHE_PATH="$V211_LOCAL_ROOT/cuda-cache"
+
+mkdir -p \
+  "$TMPDIR" \
+  "$PIP_CACHE_DIR" \
+  "$TORCH_HOME" \
+  "$TORCHINDUCTOR_CACHE_DIR" \
+  "$XDG_CACHE_HOME" \
+  "$CUDA_CACHE_PATH" \
+  "$V211_LOCAL_ROOT/work"
+
+# shellcheck disable=SC1091
+source "$V211_LOCAL_ROOT/venv/bin/activate"
+cd "$V211_REPO"
